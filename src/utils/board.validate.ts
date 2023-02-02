0686520aab5744618938fc2ee48a9297a9686424
Stanislav Mamulin <StanislavPM@ya.ru>
2023-02-02 22:43:13 +0500

    fix: correct board validation during update

--- src/utils/board.validate.ts
+++ src/utils/board.validate.ts
@@ -2,11 +2,8 @@ import { BadRequestError, BoardBadRequestError, BoardValidationError } from "../
 import { IBoard } from "../services/Board.model";
 
 export const boardValidate = (board: IBoard) => {
-    const requiredFields = ['nameBoard', 'descriptionBoard', 'columns'].sort();
-    const boardFields = Object.keys(board).sort();
-    if (JSON.stringify(requiredFields) !== JSON.stringify(boardFields)) throw new BadRequestError('field');
+    const { nameBoard, descriptionBoard } = board;
 
-    if (typeof board.nameBoard !== 'string') throw new BoardBadRequestError(BoardValidationError.nameBoard);
-    if (typeof board.descriptionBoard !== 'string') throw new BoardBadRequestError(BoardValidationError.descriptionBoard);
-    if (!Array.isArray(board.columns)) throw new BoardBadRequestError(BoardValidationError.columns);
+    if (nameBoard && typeof board.nameBoard !== 'string') throw new BoardBadRequestError(BoardValidationError.nameBoard);
+    if (descriptionBoard && typeof board.descriptionBoard !== 'string') throw new BoardBadRequestError(BoardValidationError.descriptionBoard);
 }
