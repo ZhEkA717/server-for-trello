9949c0e517bc773fdbc6a763d5b624c1e5559ba6
Stanislav Mamulin <StanislavPM@ya.ru>
2023-02-04 14:12:03 +0500

    feat: add cors policy for preflight request

--- src/Server/createServer.ts
+++ src/Server/createServer.ts
@@ -7,7 +7,8 @@ import { HandleError } from '../Errors/handler.error';
 import { BOARD_URL, COLUMN_URL,COLUMN_URL_ID, COLUMN_URL_MOVE, TASK_URL, TASK_URL_ID, TASK_URL_MOVE } from '../utils/constants';
 import { createBoard,getBoardByID, getAllBoards, deleteBoard, updateBoard } from '../services/board/Board.router';
 import { createColumn, deleteColumn, updateColumn, getAllColumsByID,getColumnByID, moveColumnToBoard } from '../services/column/Column.router';
-import { createTask, deleteTask, updateTask, getAllTasksByIDS, getTaskByID, moveTaskToColumn} from '../services/task/Task.router';
+import { createTask, deleteTask, updateTask, getAllTasksByIDS, getTaskByID, moveTaskToColumn } from '../services/task/Task.router';
+import { preflightRequest } from '../utils/network';
 
 const SERVER_BOARDS = {
     GET: getBoardByID,
@@ -34,16 +35,17 @@ export const createServer = (port = envConfig.SERVER_PORT) => {
         const url: string | undefined = req.url;
 
         try {
-            if (url?.startsWith(BOARD_URL)) {
+            if (method === "OPTIONS") preflightRequest(req, res)
+            else if (url?.startsWith(BOARD_URL)) {
                 if (method === "GET" && url === BOARD_URL) await getAllBoards(req, res);
                 else await SERVER_BOARDS[method](req, res);
             } else if (url?.startsWith(COLUMN_URL)) {
                 if (method === "GET" && url?.startsWith(COLUMN_URL_ID)) await getColumnByID(req, res)
-                if (method === "PUT" && url?.startsWith(COLUMN_URL_MOVE)) moveColumnToBoard(req, res);
+                else if (method === "PUT" && url?.startsWith(COLUMN_URL_MOVE)) moveColumnToBoard(req, res);
                 else await SERVER_COLUMNS[method](req, res);
             } else if (url?.startsWith(TASK_URL)) {
                 if (method === "GET" && url?.startsWith(TASK_URL_ID)) getTaskByID(req, res);
-                if (method === "PUT" && url?.startsWith(TASK_URL_MOVE)) moveTaskToColumn(req, res);
+                else if (method === "PUT" && url?.startsWith(TASK_URL_MOVE)) moveTaskToColumn(req, res);
                 else await SERVER_TASKS[method](req, res);
             }else{
                 throw new NotFoundError();
--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,3 +1,5 @@
+import { RouterCallbackFunc } from '../Server/Server.types';
+
 const CORSHeaders: { [header: string]: string } = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
@@ -6,4 +8,14 @@ const CORSHeaders: { [header: string]: string } = {
 export const commonJSONResponseHeaders: { [header: string]: string } = { 
     'Content-Type': 'application/json', 
     ...CORSHeaders,
+}
+
+export const preflightRequest: RouterCallbackFunc = (req, res) => {
+    res.writeHead(204, {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST, PUT, GET, OPTIONS, DELETE",
+        "Access-Control-Max-Age": 86400,
+        "Access-Control-Allow-Headers": "*"
+    })
+    res.end();
 }
