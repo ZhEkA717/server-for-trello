ab0cae174b2ff0374cc605e470a0bd116fbf29a1
Stanislav Mamulin <StanislavPM@ya.ru>
2023-02-09 21:39:06 +0500

    build: add framework connect to be able to use middleware

--- src/Server/createServer.ts
+++ src/Server/createServer.ts
@@ -3,6 +3,7 @@ import { envConfig } from '../common/config';
 import { MethodType } from './Server.types';
 import { NotFoundError } from '../Errors/CustomErrors';
 import { HandleError } from '../Errors/handler.error';
+import connect from 'connect';
 
 import { BOARD_URL, COLUMN_URL,COLUMN_URL_ID, COLUMN_URL_MOVE, LOGIN_URL, REGISTER_URL, TASK_URL, TASK_URL_ID, TASK_URL_MOVE } from '../utils/constants';
 import { createBoard,getBoardByID, getAllBoards, deleteBoard, updateBoard } from '../services/board/Board.router';
@@ -30,39 +31,44 @@ const SERVER_TASKS = {
     PUT: updateTask
 }
 
+const app = connect();
+
+
 export const createServer = (port = envConfig.SERVER_PORT) => {
-    const server = http.createServer(async (req, res) => {
-        const method = req.method as MethodType;
-        const url: string | undefined = req.url;
-
-        try {
-            if (method === "OPTIONS") preflightRequest(req, res)
-            else if (url?.startsWith(BOARD_URL)) {
-                if (method === "GET" && url === BOARD_URL) await getAllBoards(req, res);
-                else await SERVER_BOARDS[method](req, res);
-            } else if (url?.startsWith(COLUMN_URL)) {
-                if (method === "GET" && url?.startsWith(COLUMN_URL_ID)) await getColumnByID(req, res)
-                else if (method === "PUT" && url?.startsWith(COLUMN_URL_MOVE)) moveColumnToBoard(req, res);
-                else await SERVER_COLUMNS[method](req, res);
-            } else if (url?.startsWith(TASK_URL)) {
-                if (method === "GET" && url?.startsWith(TASK_URL_ID)) getTaskByID(req, res);
-                else if (method === "PUT" && url?.startsWith(TASK_URL_MOVE)) moveTaskToColumn(req, res);
-                else await SERVER_TASKS[method](req, res);
-            } else if (url?.startsWith(REGISTER_URL)) {
-                if (method === 'POST') userRegistration(req, res);
-            } else if (url?.startsWith(LOGIN_URL)) {
-                if (method === 'POST') userLogin(req, res);
-            } else {
-                throw new NotFoundError();
+    app.use(
+        async (req, res) => {
+            const method = req.method as MethodType;
+            const url: string | undefined = req.url;
+    
+            try {
+                if (method === "OPTIONS") preflightRequest(req, res)
+                else if (url?.startsWith(BOARD_URL)) {
+                    if (method === "GET" && url === BOARD_URL) await getAllBoards(req, res);
+                    else await SERVER_BOARDS[method](req, res);
+                } else if (url?.startsWith(COLUMN_URL)) {
+                    if (method === "GET" && url?.startsWith(COLUMN_URL_ID)) await getColumnByID(req, res)
+                    else if (method === "PUT" && url?.startsWith(COLUMN_URL_MOVE)) moveColumnToBoard(req, res);
+                    else await SERVER_COLUMNS[method](req, res);
+                } else if (url?.startsWith(TASK_URL)) {
+                    if (method === "GET" && url?.startsWith(TASK_URL_ID)) getTaskByID(req, res);
+                    else if (method === "PUT" && url?.startsWith(TASK_URL_MOVE)) moveTaskToColumn(req, res);
+                    else await SERVER_TASKS[method](req, res);
+                } else if (url?.startsWith(REGISTER_URL)) {
+                    if (method === 'POST') userRegistration(req, res);
+                } else if (url?.startsWith(LOGIN_URL)) {
+                    if (method === 'POST') userLogin(req, res);
+                } else {
+                    throw new NotFoundError();
+                }
+            } catch (err) {
+                HandleError(req, res, err);
             }
-        } catch (err) {
-            HandleError(req, res, err);
         }
-    })
+    );
 
-    server.listen(port, () => {
+    const server = app.listen(port, () => {
         console.log(`Server running at port ${port}`)
-    });
+    })
 
     return server;
 }
