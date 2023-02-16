a6d38c9b1343d56e587da1d7cfb95b8641ad9804
Stanislav Mamulin <StanislavPM@ya.ru>
2023-02-16 17:47:42 +0500

    refactor: simplicity models

--- src/services/board/Board.model.ts
+++ src/services/board/Board.model.ts
@@ -1,22 +1,10 @@
+import { IColumn } from "../column/Column.model";
+
 export interface IBoard {
     idBoard: string;
     nameBoard: string;
     dateBoard: Date;
     descriptionBoard: string;
     isChosen: boolean;
-    columns: {
-        idColumn: string;
-        nameColumn: string;
-        descriptionColumn: string;
-        tasks: {
-            idTask: string;
-            nameTask: string;
-            descriptionTask: string;
-            checkLists:{
-                idCheckBox:string;
-                nameCheckBox: string;
-                isChoose: boolean;
-            }[]
-        }[];
-    }[];
+    columns: IColumn[];
 };
