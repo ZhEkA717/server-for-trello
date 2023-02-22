5f06b40feab3d45af621fee8234b174a74151ea9
Stanislav Mamulin <StanislavPM@ya.ru>
2023-02-22 19:55:26 +0500

    feat: apply eslint rules to errors

--- src/Errors/CustomErrors.ts
+++ src/Errors/CustomErrors.ts
@@ -1,4 +1,5 @@
-import { ERROR_MESSAGES } from "./error.messages";
+/* eslint-disable max-classes-per-file */
+import { ErrorMessages } from './Error.messages';
 
 export enum BoardValidationError {
     nameBoard = 'name of board',
@@ -18,6 +19,7 @@ export enum CheckboxValidationError {
 
 export class BaseError {
     message: string;
+
     code: number;
 
     constructor(message: string, code = 500) {
@@ -27,46 +29,48 @@ export class BaseError {
 }
 
 export class NotFoundError extends BaseError {
-    constructor(message: string = ERROR_MESSAGES.NOT_FOUND) {
+    constructor(message: string = ErrorMessages.NOT_FOUND) {
         super(message, 404);
     }
 }
 
 export class InvalidToken extends BaseError {
-    constructor(message: string = ERROR_MESSAGES.INVALID_TOKEN) {
+    constructor(message: string = ErrorMessages.INVALID_TOKEN) {
         super(message, 401);
     }
 }
 
 export class ServerInternalError extends BaseError {
-    constructor(message: string = ERROR_MESSAGES.SERVER_INTERNAL) {
+    constructor(message: string = ErrorMessages.SERVER_INTERNAL) {
         super(message, 500);
     }
 }
 
 export class InvalidUUIDError extends BaseError {
     constructor(id: string) {
-        id === '' ?
-            super(`UserID is empty`, 400) :
-            super(`UserID = ${id} is invalid (not uuid)`, 400)
+        if (id === '') {
+            super(`UserID is empty`, 400);
+        } else {
+            super(`UserID = ${id} is invalid (not uuid)`, 400);
+        }
     }
 }
 
 export class NotExistUserError extends BaseError {
     constructor(id: string) {
-        super(`User with UserID = ${id} is not found`, 404)
+        super(`User with UserID = ${id} is not found`, 404);
     }
 }
 
 export class NotExistBoardError extends BaseError {
     constructor(id: string) {
-        super(`Board with BoardID = ${id} is not found`, 404)
+        super(`Board with BoardID = ${id} is not found`, 404);
     }
 }
 
 export class CrashDataBaseError extends BaseError {
     constructor() {
-        super(`Data base is corrupted\n. Please reload the App`, 400)
+        super(`Data base is corrupted\n. Please reload the App`, 400);
     }
 }
 
@@ -74,22 +78,26 @@ export class BadRequestError extends BaseError {
     constructor(option: string) {
         switch (option) {
             case 'field':
-                super(`${ERROR_MESSAGES.BAD_REQUEST}\nRequst.body does not contain required fields`, 400);
+                super(
+                    `${ErrorMessages.BAD_REQUEST}\nRequst.body does not contain required fields`,
+                    400,
+                );
                 break;
             case 'username':
-                super(`${ERROR_MESSAGES.BAD_REQUEST}\nusername must be a string`, 400);
+                super(`${ErrorMessages.BAD_REQUEST}\nusername must be a string`, 400);
                 break;
             case 'age':
-                super(`${ERROR_MESSAGES.BAD_REQUEST}\age must be a string`, 400);
+                super(`${ErrorMessages.BAD_REQUEST}\nage must be a string`, 400);
                 break;
             case 'hobbies':
-                super(`${ERROR_MESSAGES.BAD_REQUEST}\nhobbies must be an array`, 400);
+                super(`${ErrorMessages.BAD_REQUEST}\nhobbies must be an array`, 400);
                 break;
             case 'hobbiesArray':
-                super(`${ERROR_MESSAGES.BAD_REQUEST}\nhobbies should include only string`, 400);
+                super(`${ErrorMessages.BAD_REQUEST}\nhobbies should include only string`, 400);
                 break;
+            default:
+                super(`${ErrorMessages.BAD_REQUEST}`);
         }
-
     }
 }
 
@@ -97,32 +105,52 @@ export class BoardBadRequestError extends BaseError {
     constructor(option: BoardValidationError | 'field') {
         switch (option) {
             case 'field':
-                super(`${ERROR_MESSAGES.BAD_REQUEST}\nRequst.body does not contain required fields`, 400);
+                super(
+                    `${ErrorMessages.BAD_REQUEST}\nRequst.body does not contain required fields`,
+                    400,
+                );
                 break;
             case BoardValidationError.nameBoard:
-                super(`${ERROR_MESSAGES.BAD_REQUEST}\n${BoardValidationError.nameBoard} must be a string`, 400);
+                super(
+                    `${ErrorMessages.BAD_REQUEST}\n${BoardValidationError.nameBoard} must be a string`,
+                    400,
+                );
                 break;
             case BoardValidationError.descriptionBoard:
-                super(`${ERROR_MESSAGES.BAD_REQUEST}\n${BoardValidationError.descriptionBoard} must be a string`, 400);
+                super(
+                    `${ErrorMessages.BAD_REQUEST}\n${BoardValidationError.descriptionBoard} must be a string`,
+                    400,
+                );
                 break;
+            default:
+                super(`${ErrorMessages.BAD_REQUEST}`);
         }
-
     }
 }
 export class ColumnBadRequestError extends BaseError {
     constructor(option: ColumnValidationError | 'field') {
         switch (option) {
             case 'field':
-                super(`${ERROR_MESSAGES.BAD_REQUEST}\nRequst.body does not contain required fields`, 400);
+                super(
+                    `${ErrorMessages.BAD_REQUEST}\nRequst.body does not contain required fields`,
+                    400,
+                );
                 break;
             case ColumnValidationError.nameColumn:
-                super(`${ERROR_MESSAGES.BAD_REQUEST}\n${ColumnValidationError.nameColumn} must be a string`, 400);
+                super(
+                    `${ErrorMessages.BAD_REQUEST}\n${ColumnValidationError.nameColumn} must be a string`,
+                    400,
+                );
                 break;
             case ColumnValidationError.descriptionColumn:
-                super(`${ERROR_MESSAGES.BAD_REQUEST}\n${ColumnValidationError.descriptionColumn} must be a string`, 400);
+                super(
+                    `${ErrorMessages.BAD_REQUEST}\n${ColumnValidationError.descriptionColumn} must be a string`,
+                    400,
+                );
                 break;
+            default:
+                super(`${ErrorMessages.BAD_REQUEST}`);
         }
-
     }
 }
 
@@ -130,14 +158,25 @@ export class TaskBadRequestError extends BaseError {
     constructor(option: TaskValidationError | 'field') {
         switch (option) {
             case 'field':
-                super(`${ERROR_MESSAGES.BAD_REQUEST}\nRequst.body does not contain required fields`, 400);
+                super(
+                    `${ErrorMessages.BAD_REQUEST}\nRequst.body does not contain required fields`,
+                    400,
+                );
                 break;
             case TaskValidationError.nameTask:
-                super(`${ERROR_MESSAGES.BAD_REQUEST}\n${TaskValidationError.nameTask} must be a string`, 400);
+                super(
+                    `${ErrorMessages.BAD_REQUEST}\n${TaskValidationError.nameTask} must be a string`,
+                    400,
+                );
                 break;
             case TaskValidationError.descriptionTask:
-                super(`${ERROR_MESSAGES.BAD_REQUEST}\n${TaskValidationError.descriptionTask} must be a string`, 400);
+                super(
+                    `${ErrorMessages.BAD_REQUEST}\n${TaskValidationError.descriptionTask} must be a string`,
+                    400,
+                );
                 break;
+            default:
+                super(`${ErrorMessages.BAD_REQUEST}`);
         }
     }
 }
@@ -146,11 +185,19 @@ export class CheckboxBadRequestError extends BaseError {
     constructor(option: CheckboxValidationError | 'field') {
         switch (option) {
             case 'field':
-                super(`${ERROR_MESSAGES.BAD_REQUEST}\nRequst.body does not contain required fields`, 400);
+                super(
+                    `${ErrorMessages.BAD_REQUEST}\nRequst.body does not contain required fields`,
+                    400,
+                );
                 break;
             case CheckboxValidationError.nameCheckBox:
-                super(`${ERROR_MESSAGES.BAD_REQUEST}\n${CheckboxValidationError.nameCheckBox} must be a string`, 400);
+                super(
+                    `${ErrorMessages.BAD_REQUEST}\n${CheckboxValidationError.nameCheckBox} must be a string`,
+                    400,
+                );
                 break;
+            default:
+                super(`${ErrorMessages.BAD_REQUEST}`);
         }
     }
 }
--- src/common/config.ts
+++ src/common/config.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { IEnvConfig } from './config.interface';
 
 const dotenvConfig = dotenv.config({
-  path: path.join(__dirname, '../../.env')
+    path: path.join(__dirname, '../../.env'),
 });
 
 const envConfig = dotenvConfig.parsed as unknown as IEnvConfig;
