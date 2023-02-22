31ca0e11f0c267e2f043e878689fa0b9ea107e35
Stanislav Mamulin <StanislavPM@ya.ru>
2023-02-22 19:17:09 +0500

    feat: apply eslint rules to server

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,5 +36,6 @@ module.exports = {
         ],
         'import/extensions': 'off',
         'no-param-reassign': ['error', { props: false }],
+        'import/prefer-default-export': 'off',
     },
 };
